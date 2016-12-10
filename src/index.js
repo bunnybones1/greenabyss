0304f5235709ca68066d7b0a6ea97d3adca52a64
Tomasz Dysinski <tom@jam3.com>
2016-12-09 19:46:28 -0500

    src/node_modules is a deliberate inclusion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 var version = require('./version.json');
 console.log('green abyss client bundle version: ' + version.gitIdString);
 
+var GameWorld = require('GameWorld');
+var View = require('threejs-managed-view').View;
 var googleApi = require('./googleApi')
 
 googleApi.init({
 	client_id: '1003887978766-ks5t12lss4gehrtbvcidpau0lj6vs0nf.apps.googleusercontent.com'
 });
 
-var view = new (require('threejs-managed-view').View)();
+var view = new View();
 
+require('utils/manageSceneOnEnterFrameObjects')(view.scene);
+view.renderManager.onEnterFrame.add(view.scene.onEnterFrame);
 
+var gameWorld = new GameWorld({
+	scene: view.scene
+});
