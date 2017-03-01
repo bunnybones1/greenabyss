f808b120547ad143ec18f13df2706a098fe6ecb7
Tomasz Dysinski <tom@jam3.com>
2017-03-01 00:30:37 -0500

    collision logic and jumping between magic window and cardboard, as well as prep for daydream mode

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ var WebVRPolyfill = require('webvr-polyfill');
 var App = require('ThreeAppBase');
 var GameWorld = require('GameWorld');
 var googleApi = require('./googleApi');
-var onVR = require('utils/devices/onVR');
 
 googleApi.init({
 	client_id: '1003887978766-ks5t12lss4gehrtbvcidpau0lj6vs0nf.apps.googleusercontent.com'
@@ -21,13 +20,11 @@ function onLoad() {
 	var app = new App();
 
 	var gameWorld = new GameWorld({
+		app: app,
 		scene: app.scene,
-		renderer: app.renderer
+		renderer: app.renderer,
+		rayEvents: app.rayEvents
 	});
-	function onVRChangeCamera(state) {
-		app.camera = state ? gameWorld.cameraFirstPerson : gameWorld.cameraThirdPerson;
-	}
-	onVR(onVRChangeCamera);
 	app.registerOnEnterFrame(gameWorld.onEnterFrame.bind(gameWorld));
 	app.registerOnResize(gameWorld.onResize.bind(gameWorld));
 
